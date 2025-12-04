30e9d0ae9494fe80ac90ae461733036cf81f4843
yyy1029 <116483915+yyy1029@users.noreply.github.com>
2025-12-04 01:29:33 -0800

    Update app.py

--- Amber_design3/app.py
+++ Amber_design3/app.py
@@ -436,9 +436,9 @@ with main_col_right:
                         if should_trigger_spinner: loading_message_placeholder.empty()
                         st.error("Map data processing failed.")
                     else:
-                                                if RATIO_COL not in df_zip_map.columns:
-                            denom_zip = df_zip_map[income_col].replace(0, np.nan)
-                            df_zip_map[RATIO_COL] = df_zip_map[price_col] / denom_zip
+                        if RATIO_COL not in df_zip_map.columns:
+                        denom_zip = df_zip_map[income_col].replace(0, np.nan)
+                        df_zip_map[RATIO_COL] = df_zip_map[price_col] / denom_zip
                         
                         df_zip_map["affordability_rating"] = df_zip_map[RATIO_COL].apply(classify_affordability)
                         
