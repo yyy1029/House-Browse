b37ab1b35f61785c3e6181dfab39847447753edd
yyy1029 <116483915+yyy1029@users.noreply.github.com>
2025-12-04 02:14:30 -0800

    Update app.py

--- Amber_design3/app.py
+++ Amber_design3/app.py
@@ -1021,14 +1021,14 @@ with main_col_right:
                     # st.markdown(
                     #     f"""
                     #     - Median sale price: **${row['Median Sale Price']:,.0f}**
-                    #     - Household Income: **${row['Household Income']:,.0f}**
+                    #     - Per-capita income: **${row['Per Capita Income']:,.0f}**
                     #     - Metro-area price-to-income ratio: **{row[RATIO_COL]:.2f}**
                     #     - **Affordability Rating:** **{row['affordability_rating']}** """
                     # )
                     st.markdown(
                         f"""
                         - Median sale price: **${row['Median Sale Price']:,.0f}**
-                        - Per-capita income: **${row['Per-capita income']:,.0f}**
+                        - Per-capita income: **${row['Per Capita Income']:,.0f}**
                          """
                     )
 
