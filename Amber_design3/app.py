a2a70bdd8dfb40340c044c1fa62874ce111075df
yyy1029 <116483915+yyy1029@users.noreply.github.com>
2025-12-04 02:13:29 -0800

    Update app.py

--- Amber_design3/app.py
+++ Amber_design3/app.py
@@ -1028,7 +1028,7 @@ with main_col_right:
                     st.markdown(
                         f"""
                         - Median sale price: **${row['Median Sale Price']:,.0f}**
-                        - Household Income: **${row['Per-capita income']:,.0f}**
+                        - Per-capita income: **${row['Per-capita income']:,.0f}**
                          """
                     )
 
